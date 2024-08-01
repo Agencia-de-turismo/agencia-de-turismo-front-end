c2f8aaacc11f3b044522a4c8832e49e718f7a27c
Justin Huertas <justin.hv08@gmail.com>
2024-07-31 20:50:30 -0500

    modificar ruta principal al login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,8 @@
 export const LoginPage = () => {
-	return <h1>LoginPage</h1>;
+	return (
+		<div className='flex items-center justify-center w-full h-screen'>
+			<h1 className='text-5xl font-bold'>Login Page</h1>
+		</div>
+	);
 };
+
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-
-import { HomeLayout } from "../tourism/layouts/HomeLayout";
 import { AuthLayout } from "../auth/layouts/AuthLayout";
-
 import { ErrorPage } from "../tourism/pages/ErrorPage";
-import { HomePage } from "../tourism/pages/HomePage";
-
-import { RegisterPage } from "../auth/pages/RegisterPage";
 import { LoginPage } from "../auth/pages/LoginPage";
 
 
@@ -14,28 +8,13 @@ export const router = createBrowserRouter([
 	{
 		path: "/",
 		errorElement: <ErrorPage/>,
-		element: <HomeLayout/>,
+		element: <AuthLayout />,
 		children: [
 			{
 				path: "/",
 				index: true,
-				element: <HomePage/>
-			}
-		]
-	},
-	{
-		path: "/auth",
-		errorElement: <ErrorPage/>,
-		element: <AuthLayout />,
-		children: [
-			{
-				path: "/auth/login",
 				element: <LoginPage />
 			},
-			{
-				path: "/auth/register",
-				element: <RegisterPage />
-			}
 		]
 	}
 ])
