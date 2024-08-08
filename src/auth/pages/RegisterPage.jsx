0864ebdc797a4757b4f6bf246351433deb40ffd7
Emily <galvezemily0820@gmail.com>
2024-08-07 21:13:56 -0500

    -

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,1 @@
-export const RegisterPage = () => {
-	return (
-		<div className="flex items-center justify-center w-full h-screen">
-			<h1 className="text-5xl font-bold">RegisterPage</h1>
-		</div>
-	);
-};
+
